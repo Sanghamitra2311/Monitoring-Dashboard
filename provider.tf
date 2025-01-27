a28572d852c315f404324b76bbe25b12cb19e79d
Sanghamitra2311 <87751059+Sanghamitra2311@users.noreply.github.com>
2025-01-27 17:35:03 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project="my-project-a1-31774"
+  region="us-central1"
+}
